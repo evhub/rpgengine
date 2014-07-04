dc929d55e7c8ce78fad4d7f06e69e97cc7789524
evhub <evanjhub@gmail.com>
2014-07-04 00:58:55 -0700

    Adds the make command

--- RPGEngine.py
+++ RPGEngine.py
@@ -368,6 +368,7 @@ Import Commands:
             self.cmd_do,
             self.cmd_show,
             self.cmd_del,
+            self.cmd_make,
             self.cmd_def,
             self.cmd_set,
             self.cmd_normal
